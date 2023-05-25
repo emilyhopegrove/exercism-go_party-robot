6098aa643f2d8a85cbe5ea56e88e366067592d85
Emily Grove <eh.grove@yahoo.com>
2023-05-25 17:51:15 -0500

    Working Solutions -- had issues with the final function passing tests which was fixed with a space between middle messages

--- party_robot.go
+++ party_robot.go
@@ -1,16 +1,33 @@
 package partyrobot
+import "fmt"
 
 // Welcome greets a person by name.
 func Welcome(name string) string {
-	panic("Please implement the Welcome function")
+	return fmt.Sprintf("Welcome to my party, %s!", name)
 }
 
 // HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
 func HappyBirthday(name string, age int) string {
-	panic("Please implement the HappyBirthday function")
+	return fmt.Sprintf("Happy birthday %s! You are now %v years old!", name, age)
 }
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	panic("Please implement the AssignTable function")
+	// Generate a welcome message for the guest
+	welcomeMessage := Welcome(name)
+
+	// Generate the table assignment message
+	tableAssignment := fmt.Sprintf("You have been assigned to table %03d.", table)
+
+	// Generate the table location message
+	tableLocation := fmt.Sprintf("Your table is %s, exactly %.1f meters from here.", direction, distance)
+	
+	// Generate the neighbor information message
+	neighborInfo := fmt.Sprintf("You will be sitting next to %s.", neighbor)
+
+	// Combine all the messages to create the final output, add space between the middle 2 messages to pass tests
+	return fmt.Sprintf("%s\n%s %s\n%s", welcomeMessage, tableAssignment, tableLocation, neighborInfo)
+	
 }
+
+
